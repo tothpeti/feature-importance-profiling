71f4b068af4c53b00c2ed071863af6ceced6314a
tothpeti <toth.peter96@digikabel.hu>
2021-02-06 11:01:58 +0100

    Added requirements and started structuring FeatureImportanceProfiler

--- src/profiler/FeatureImportanceProfiler.py
+++ src/profiler/FeatureImportanceProfiler.py
@@ -1,5 +1,13 @@
+import pandas as pd
 
-class FeatureImportanceProfiler():
 
-    def __init__(self):
+class FeatureImportanceProfiler:
+
+    def __init__(self, dataset: pd.DataFrame):
+        self.dataset = dataset
+
+    def visualize(self):
+        pass
+
+    def return_filtered_dataset(self):
         pass
