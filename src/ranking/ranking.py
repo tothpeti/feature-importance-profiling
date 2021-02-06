5b3824103ec054d0052b90fa9a37dd0456bed885
tothpeti <toth.peter96@digikabel.hu>
2021-02-06 11:54:49 +0100

    Created ranking.py

---
+++ src/ranking/ranking.py
@@ -0,0 +1,17 @@
+
+class Ranking:
+    @classmethod
+    def avg_rank(cls):
+        pass
+
+    @classmethod
+    def success_rate_ratio(cls):
+        pass
+
+    @classmethod
+    def borda_count(cls):
+        pass
+
+    @classmethod
+    def instant_runoff(cls):
+        pass
