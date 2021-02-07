347dfc69bfd4e7574948a950689abce63f35d860
tothpeti <toth.peter96@digikabel.hu>
2021-02-07 14:14:59 +0100

    Created BaseFeatureImportanceAccessor and renamed a class

---
+++ src/accessor/BaseFeatureImportanceAccessor.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class BaseFeatureImportanceAccessor(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def fi_random_forest(x_train: Any, y_train: Any):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fi_xgboost(x_train: Any, y_train: Any):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fi_mutual_information(x_train: Any, y_train: Any):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fi_logistic_regression(x_train: Any, y_train: Any):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def fi_extra_trees(x_train: Any, y_train: Any):
+        pass
--- src/accessor/FeatureImportanceAccessor.py
+++ src/accessor/FeatureImportanceAccessor.py
@@ -4,33 +4,35 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import mutual_info_classif
 from xgboost.sklearn import XGBClassifier
 
+from accessor import BaseFeatureImportanceAccessor
 
-class FeatureImportanceAccessor:
-    @classmethod
-    def fi_random_forest(cls, x_train: Any, y_train: Any) -> Any:
+
+class BinaryFeatureImportanceAccessor(BaseFeatureImportanceAccessor):
+    @staticmethod
+    def fi_random_forest(x_train: Any, y_train: Any) -> Any:
         model = RandomForestClassifier(n_estimators=100, criterion="gini")
         model.fit(x_train, y_train)
         return model.feature_importances_
 
-    @classmethod
-    def fi_xgboost_binary(cls, x_train: Any, y_train: Any) -> Any:
+    @staticmethod
+    def fi_xgboost(x_train: Any, y_train: Any) -> Any:
         model = XGBClassifier(objective="binary:logistic")
         model.fit(x_train, y_train)
         return model.feature_importances_
 
-    @classmethod
-    def fi_mutual_information(cls, x_train: Any, y_train: Any) -> Any:
+    @staticmethod
+    def fi_mutual_information(x_train: Any, y_train: Any) -> Any:
         tmp = [mutual_info_classif(x_train[col].values.reshape(-1, 1), y_train) for col in x_train.columns]
         return [value[0] for value in tmp]
 
-    @classmethod
-    def fi_logistic_regression(cls, x_train: Any, y_train: Any) -> Any:
+    @staticmethod
+    def fi_logistic_regression(x_train: Any, y_train: Any) -> Any:
         model = LogisticRegression()
         model.fit(x_train, y_train)
         return model.coef_[0]
 
-    @classmethod
-    def fi_extra_trees(cls, x_train: Any, y_train: Any) -> Any:
+    @staticmethod
+    def fi_extra_trees(x_train: Any, y_train: Any) -> Any:
         model = ExtraTreesClassifier()
         model.fit(x_train, y_train)
         return model.feature_importances_
