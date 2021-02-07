cae1b0c95fd127202ddf216a1b828bf466100131
tothpeti <toth.peter96@digikabel.hu>
2021-02-07 11:43:30 +0100

    Created FeatureImportanceAccessor class

---
+++ src/accessor/FeatureImportanceAccessor.py
@@ -0,0 +1,21 @@
+
+class FeatureImportanceAccessor:
+    @classmethod
+    def fi_random_forest(cls, x_train, y_train):
+        pass
+
+    @classmethod
+    def fi_xgboost(cls, x_train, y_train):
+        pass
+
+    @classmethod
+    def fi_mutual_information(cls, x_train, y_train):
+        pass
+
+    @classmethod
+    def fi_logistic_regression(cls, x_train, y_train):
+        pass
+
+    @classmethod
+    def fi_extra_trees(cls, x_train, y_train):
+        pass
--- src/profiler/profiler.py
+++ src/profiler/profiler.py
@@ -1,10 +1,13 @@
-import pandas as pd
+from typing import Any
 
 
 class FeatureImportanceProfiler:
 
-    def __init__(self, dataset: pd.DataFrame):
-        self.dataset = dataset
+    def __init__(self, x_train: Any, y_train: Any, x_test: Any, y_test: Any):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
 
     def visualize(self):
         pass
