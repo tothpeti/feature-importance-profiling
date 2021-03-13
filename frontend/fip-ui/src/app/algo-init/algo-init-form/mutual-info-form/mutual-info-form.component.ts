73c296d12c46ee62505e9403cef6303a08f1ec7b
tothpeti <toth.peter96@digikabel.hu>
2021-03-13 17:04:59 +0100

    Implemented result card and form for xgboost algorithm

--- frontend/fip-ui/src/app/algo-init/algo-init-form/mutual-info-form/mutual-info-form.component.ts
+++ frontend/fip-ui/src/app/algo-init/algo-init-form/mutual-info-form/mutual-info-form.component.ts
@@ -2,7 +2,6 @@ import {Component, Input, OnInit} from '@angular/core';
 import {IAlgorithm} from '../../../shared/model/algo-interface.model';
 import {InMemoryDataStoreService} from '../../../shared/service/in-memory-data-store.service';
 import {MutualInformationModel} from '../../../shared/model/mutual-information.model';
-import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-mutual-info-form',
@@ -12,24 +11,11 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class MutualInfoFormComponent implements OnInit {
 
   @Input() selectedDefaultAlgorithm: IAlgorithm;
-  // algorithmForm: FormGroup;
 
   constructor(private inMemoryDataStoreService: InMemoryDataStoreService) { }
 
   ngOnInit(): void {
-    // this.initForm();
   }
-
-  /*
-  initForm(): void {
-    const algorithmName = this.selectedDefaultAlgorithm.algoName;
-    this.algorithmForm = new FormGroup({
-      name: new FormControl(algorithmName)
-    });
-    console.log(this.algorithmForm.value.name);
-  }
-
-   */
 
   onSubmit(): void {
     const newAlgo = new MutualInformationModel();
--- frontend/fip-ui/src/app/algo-init/algo-init-form/xgboost-form/xgboost-form.component.ts
+++ frontend/fip-ui/src/app/algo-init/algo-init-form/xgboost-form/xgboost-form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {IAlgorithm} from '../../../shared/model/algo-interface.model';
+import {XgboostModel} from "../../../shared/model/xgboost.model";
+import {InMemoryDataStoreService} from "../../../shared/service/in-memory-data-store.service";
 
 @Component({
   selector: 'app-xgboost-form',
@@ -7,12 +10,63 @@ import {FormGroup} from '@angular/forms';
   styleUrls: ['./xgboost-form.component.css']
 })
 export class XgboostFormComponent implements OnInit {
-
+  @Input() selectedDefaultAlgorithm: IAlgorithm;
   algorithmForm: FormGroup;
 
-  constructor() { }
+  constructor(private inMemoryDataStoreService: InMemoryDataStoreService) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+     const name = this.selectedDefaultAlgorithm.algoName;
+     let eta = this.selectedDefaultAlgorithm.eta;
+     let maxDepth = this.selectedDefaultAlgorithm.maxDepth;
+     let minChildWeight = this.selectedDefaultAlgorithm.minChildWeight;
+     let gamma = this.selectedDefaultAlgorithm.gamma;
+     let subsample = this.selectedDefaultAlgorithm.subsample;
+     let colsampleByTree = this.selectedDefaultAlgorithm.colsampleByTree;
+     let alpha = this.selectedDefaultAlgorithm.alpha;
+
+     this.algorithmForm = new FormGroup({
+       name: new FormControl(name),
+       eta: new FormControl(eta),
+       maxDepth: new FormControl(maxDepth),
+       minChildWeight: new FormControl(minChildWeight),
+       gamma: new FormControl(gamma),
+       subsample: new FormControl(subsample),
+       colsampleByTree: new FormControl(colsampleByTree),
+       alpha: new FormControl(alpha)
+     });
+  }
+
+  onReset(): void {
+    this.algorithmForm.setValue({
+      name: this.selectedDefaultAlgorithm.algoName,
+      eta: this.selectedDefaultAlgorithm.eta,
+      maxDepth: this.selectedDefaultAlgorithm.maxDepth,
+      minChildWeight: this.selectedDefaultAlgorithm.minChildWeight,
+      gamma: this.selectedDefaultAlgorithm.gamma,
+      subsample: this.selectedDefaultAlgorithm.subsample,
+      colsampleByTree: this.selectedDefaultAlgorithm.colsampleByTree,
+      alpha: this.selectedDefaultAlgorithm.alpha
+    });
+  }
+
+  onSubmit(): void {
+    const newAlgo = new XgboostModel(
+      this.algorithmForm.value.eta,
+      this.algorithmForm.value.maxDepth,
+      this.algorithmForm.value.minChildWeight,
+      this.algorithmForm.value.gamma,
+      this.algorithmForm.value.subsample,
+      this.algorithmForm.value.colsampleByTree,
+      this.algorithmForm.value.alpha
+    );
+
+    this.inMemoryDataStoreService.addInitializedAlgorithm(newAlgo);
+    this.onReset();
   }
 
 }
